41823df78babc8503f2e5aca196a4e7fbf9f71ac
SadaafChowdhury <c.sadaaf@gmail.com>
2019-07-12 19:46:54 -0700

    Feature:  Connected Registration page with login page
    #18

--- PROJECT CODE/Tracker/app/src/main/java/com/example/tracker/MainActivity.java
+++ PROJECT CODE/Tracker/app/src/main/java/com/example/tracker/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.tracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button Registration_Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Registration_Button=(Button) findViewById(R.id.Registration_Button);
+        Registration_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegistration_Page();
+            }
+        });
+    }
+    public void openRegistration_Page(){
+        Intent intent = new Intent(this, Registration_Page.class);
+        startActivity(intent);
     }
 }
--- PROJECT CODE/Tracker/app/src/main/java/com/example/tracker/Registration_Page.java
+++ PROJECT CODE/Tracker/app/src/main/java/com/example/tracker/Registration_Page.java
@@ -1,13 +1,29 @@
 package com.example.tracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Registration_Page extends AppCompatActivity {
+    private Button LoginPage_Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration__page);
+
+        LoginPage_Button = (Button) findViewById(R.id.LoginPage_Button);
+        LoginPage_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin_Page();
+            }
+        });
+    }
+    public void openLogin_Page(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
