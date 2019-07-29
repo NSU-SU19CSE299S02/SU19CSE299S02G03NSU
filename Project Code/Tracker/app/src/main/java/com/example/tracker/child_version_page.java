dfa4dcd7d2729830d1c08df98adbb1aa77cfc31e
SadaafChowdhury <c.sadaaf@gmail.com>
2019-07-29 14:57:53 -0700

    Bug #19 : Added the project folder and renamed it
    renamed the project folder

---
+++ Project Code/Tracker/app/src/main/java/com/example/tracker/child_version_page.java
@@ -0,0 +1,13 @@
+package com.example.tracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class child_version_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child_version_page);
+    }
+}
